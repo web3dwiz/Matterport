f7717f960a55a20a53df8777fbdec2c13ecd8b2a
web3dwiz <core-solver@hotmail.com>
2024-02-27 21:07:00 -0500

    update

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,57 @@ import { useEffect, useRef, useState } from "react";
 import "./App.css";
 import { MpSdk, setupSdk } from "@matterport/sdk";
 
+// "https://my.matterport.com/show?qs=1&m=j4RZx7ZGM6T&play=1&applicationKey=5kic6ie6efzhec229s1783usc"
+
+function getRandomInteger(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
 function App() {
   const [sdk, setSdk] = useState<MpSdk>();
-  const [horizontal, setHorizontal] = useState(45);
-  const [vertical, setVertical] = useState(15);
   const container = useRef<HTMLDivElement>(null);
   let started = false;
+
+  const intersectionRef = useRef<any>();
 
   useEffect(() => {
     if (!started && container.current) {
       started = true;
-      setupSdk("5kic6ie6efzhec229s1783usc", {
-        container: container.current,
-        space: "j4RZx7ZGM6T",
-        iframeQueryParams: { qs: 1 },
-      }).then((mpSDK: MpSdk) => {
-        setSdk(mpSDK);
-      });
+
+      (async () => {
+        const mpSdk = await setupSdk("5kic6ie6efzhec229s1783usc", {
+          container: container.current!,
+          space: "j4RZx7ZGM6T",
+          iframeQueryParams: { qs: 1 },
+        });
+
+        await mpSdk.App.state.waitUntil(
+          (state: any) => state.phase === mpSdk.App.Phase.PLAYING
+        );
+
+        mpSdk.Pointer.intersection.subscribe(function (intersection: any) {
+          intersectionRef.current = intersection;
+        });
+
+        const modelData = await mpSdk.Model.getData();
+
+        // Get random Sweep ID
+        const randomID = getRandomInteger(0, modelData.sweeps.length);
+        let { sid, rotation, floor } = modelData.sweeps[randomID];
+        console.log(modelData, modelData.sweeps);
+
+        // mpSdk.Camera.rotate(135, 0);
+
+        if (floor) {
+          await mpSdk.Sweep.moveTo(sid, {
+            rotation: rotation,
+            transition: mpSdk.Sweep.Transition.INSTANT,
+            transitionTime: 2000,
+          });
+        }
+
+        setSdk(mpSdk);
+      })();
     }
   }, []);
 
